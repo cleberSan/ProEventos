ad97fb5104bdfe9cb4ea638ab5e89b391f3da8d8
Cleber Costa Santos <cleber.santos@dorconsultoria.com.br>
2022-08-26 00:19:24 -0300

    Seção 3: Introdução ao EF Core

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,42 +13,24 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public IEnumerable<Evento> _evento = new Evento[] {
-            new Evento() {
-                EventoId = 1,
-                Tema = "Angular 12 e .NET 5",
-                Local = "São Paulo",
-                Lote = "1º Lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImagemURL = "foto.png"
-            },
-
-            new Evento() {
-                EventoId = 2,
-                Tema = "React.JS e React.Native + Docker",
-                Local = "Rio de Janeiro",
-                Lote = "2º Lote",
-                QtdPessoas = 340,
-                DataEvento = DateTime.Now.AddDays(6).ToString("dd/MM/yyyy"),
-                ImagemURL = "image.jpg"
-            }
-        };
-        public EventoController()
+        private readonly DataContext context;
+        public EventoController(DataContext context)
         {
-            
+            this.context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return context.Eventos.FirstOrDefault(
+                evento => evento.EventoId == id
+            );
         }
 
         [HttpPost]
